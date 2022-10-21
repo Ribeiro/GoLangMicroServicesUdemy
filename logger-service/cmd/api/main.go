d0dee0b8869bf5684aa48af8ddcc6c53b25a3eeb
Geovanny Ribeiro <geovanny.ribeiro@gmail.com>
2022-10-21 08:47:03 -0300

    Implemented Logger Service

---
+++ logger-service/cmd/api/main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"log-service/data"
+	"net/http"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const webPort = "80"
+const mongoUrl = "mongodb://mongo:27017"
+const rpcPort = "5001"
+const gRpcPort = "50001"
+
+var client *mongo.Client
+
+type Config struct {
+	Models data.Models
+}
+
+func main() {
+	//Connect to Mongo
+	mongoClient, err := connectToMongo()
+	if err != nil {
+		log.Panic(err)
+	}
+	client = mongoClient
+
+	//create context in order to disconnect
+	ctx, cancel := context.WithTimeout((context.Background()), 15*time.Second)
+	defer cancel()
+
+	//close connection
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+
+	app := Config{
+		Models: data.New(client),
+	}
+
+	//Start web server
+	//go app.serve()
+	log.Println("Starting Log Service on port:", webPort)
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	err = srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+// func (app *Config) serve() {
+// 	srv := &http.Server{
+// 		Addr:    fmt.Sprintf(":%s", webPort),
+// 		Handler: app.routes(),
+// 	}
+
+// 	err := srv.ListenAndServe()
+// 	if err != nil {
+// 		log.Panic(err)
+// 	}
+// }
+
+func connectToMongo() (*mongo.Client, error) {
+	//create connection options
+	clientOptions := options.Client().ApplyURI(mongoUrl)
+	clientOptions.SetAuth(options.Credential{
+		Username: "admin",
+		Password: "admin",
+	})
+
+	//connect
+	c, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Println("Error connecting: ", err)
+		return nil, err
+	}
+
+	log.Println("Connected to MongoDB")
+
+	return c, nil
+}
---
+++ logger-service/logger-service.dockerfile
@@ -0,0 +1,10 @@
+#build a tiny docker image
+FROM alpine:latest
+
+RUN mkdir /app
+
+COPY loggerApp /app
+
+CMD [ "/app/loggerApp" ]
+
+
